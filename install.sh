82b5da75ed11f66acf1d662093a47ca68aac5fcb
faroke <faroke-wb@outlook.fr>
2016-03-13 20:49:31 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#Script By Obscurcia
+#Naoux and FaRoKe
+clear
+
+sudo apt-get install -y nmap
+sudo apt-get install -y macchanger
+sudo apt-get install -y lynis
+sudo apt-get install -y fail2ban
+sudo apt-get install -y iptables
+sudo apt-get install -y portsentry
+sudo apt-get install -y logwatch
+sudo apt-get install -y proxychains
+sudo apt-get install -y tor
+sudo apt-get install -y oinkmaster snort snort-rules-default
+
