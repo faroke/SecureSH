e6468fce9a5d1d4b501f56e2487c1d4f010cd91a
faroke <faroke-wb@outlook.fr>
2018-05-30 19:19:35 +0200

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -1,1 +1,28 @@
-
+#!/bin/bash
+clear
+menu () { 
+read -p "[>] Quel est ton choix ?
+         [1] Lynis
+         [2] Rkhunter
+         [Q] Exit
+>" action
+}
+menu 
+  if [ $action= '1' ]
+  then
+      clear
+      sh lynis.sh
+  elif [ $action '2' ]
+  then
+       echo "not yet amigo"
+  elif [ $action 'Q' ]
+  then
+      exit
+  else
+  		echo "Error: Veuillez entrer un choix valide !"
+		read -p " >>>" mainchoice	
+	fi
+	fi
+	fi
+clear
+exit
