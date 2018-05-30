0862c4f2d73b6eff41c05f2cc8237ff4235a596d
faroke <faroke-wb@outlook.fr>
2018-05-30 18:56:19 +0200

    Update monitorwww.sh

--- monitorwww.sh
+++ monitorwww.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-#A MODIFIER
 echo -e "----------------------------------------------------------
 Programme de surveillance des fichiers PHP, HTML, HTACCESS & JS
-v 2.0
-Editeur : https://www.kanjian.fr
 ---------------------------------------------------------- "
 
-Subject="[SRV-001] Rapport surveillance des fichiers : php, html, js, conf, htaccess"
+Subject="Rapport surveillance des fichiers : php, html, js, conf, htaccess"
 
 find /var/www/ -name -ls -o -regex '.*.(php|htaccess|conf|html|js).*' -mtime 0 | xargs ls -lah > liste-fichiers-modifies.txt
 
-mail -s "$Subject" "inazo@example.com" < liste-fichiers-modifies.txt
+mail -s "$Subject" "isncarrymybac@gmail.com" < liste-fichiers-modifies.txt
