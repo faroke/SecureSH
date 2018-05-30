7dc6c16ab02057de56a3c4eb57eb29967dd6b05d
faroke <faroke-wb@outlook.fr>
2018-05-30 18:54:03 +0200

    Update lynis.sh

--- lynis.sh
+++ lynis.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 #Script Lynis by Naoux
 clear
+menu () {
 read -p "[>] Que veux-tu faire ?
 	 [1] Faire un scan
 	 [Q] Quitter
 > " action
+}
 
 	if [ $action = "1" ]
 	then
 		lynis --check-all
 		clear
 sleep 1
-read -p "[>] Que veux-tu faire ?
-	 [1] Faire un scan
-	 [Q] Quitter
-> " action
+menu
 fi 
 	
 	if [ $action = "Q" ] || [ $action = "q" ]
