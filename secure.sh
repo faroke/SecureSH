02112ea0dae47f13b395327fa6801dad6d5e3bb2
faroke <faroke-wb@outlook.fr>
2018-05-30 19:09:39 +0200

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -1,18 +1,36 @@
 #!/bin/sh
 #Script by Faroke
 clear
+	cat cat.txt
 	echo "Bienvenue sur Subuntu"
-sleep 1 
-	echo "Attention, l'outil vérifie que tout est bien parametrer avant le lancement..."
-sleep 1
-	cat "cat.txt"
+	echo "1. Anti-Bruteforcing"
+	echo "2. Scan de la Machine"
+	echo "3. Anonyma"
+	echo "4. Protection Anti-scan"
+	echo ""
+
+
 read -p " >>>" mainchoice
 	if [ $mainchoice = "1" ]
 	then
-		echo "Première Action"
+		sh fail2ban.sh
+	elif [ $mainchoice = "2" ]
+	then
+		sh scanner.sh
+	elif [ $mainchoice = "3" ]
+	then
+		sh anonyma.sh
+	elif [ $mainchoice = "4" ]
+	then
+		sh portsentry.sh
 	else
+	
+	
 		echo "Error: Veuillez entrer un choix valide !"
 		read -p " >>>" mainchoice	
+	fi
+	fi
+	fi
 	fi
 clear
 exit
