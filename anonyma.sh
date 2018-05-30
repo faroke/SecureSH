48225d0dab98bf2cfe4f0a416d80bdc02dfe0334
faroke <faroke-wb@outlook.fr>
2018-05-30 19:14:17 +0200

    Update anonyma.sh

--- anonyma.sh
+++ anonyma.sh
@@ -1,1 +1,29 @@
-
+#!/bin/bash
+clear
+menu () { 
+read -p "[>] Quel est ton choix ?
+         [1] proxychains
+         [2] Macchanger
+         [Q] Exit
+>" action
+}
+menu 
+  if [ $action= '1' ]
+  then
+      clear
+      sh proxychains.sh
+  elif [ $action '2' ]
+  then
+       clear sh macchanger.sh
+  elif [ $action 'Q' ]
+  then
+      exit
+  else
+  		echo "Error: Veuillez entrer un choix valide !"
+		read -p " >>>" mainchoice	
+	fi
+	fi
+	fi
+clear
+exit
+  
